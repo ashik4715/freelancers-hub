ac28248b0cfb66eaf8f9f5768a4bc115a3be3f1e
ashik4715 <ashikurjhalak@gmail.com>
2022-03-21 13:26:04 +0600

    my skill fixing

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ $(function() {
     });
 });
 
+// Slide MY SKILL List items
+$(document).ready(function(){
+    $('.button').on('click')
+        $('selected-skill').slideToggle(); 
+});
+
 
 // const doc = new jsPDF();
 // var specialElementHandlers = {
